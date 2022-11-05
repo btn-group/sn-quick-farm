e471317ec52f14330de2cea5590d002a4e991a0d
Steven Chang <steven.chang@btn.group>
2022-11-05 19:10:57 +1000

    Test receive init swap and provide (#12)

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const BLOCK_SIZE: usize = 1;
+pub const BLOCK_SIZE: usize = 256;
 pub const CONFIG_KEY: &[u8] = b"config";
 pub const MOCK_AMOUNT: u128 = 1_000_000_000_000;
 pub const MOCK_BUTT_ADDRESS: &str = "mock-butt-address";
