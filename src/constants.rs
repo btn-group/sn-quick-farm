ce2a8736000a801339519dd25f1567a5d1b6f375
Steven Chang <stevenchang5000@gmail.com>
2022-11-05 16:22:24 +1000

    Enable admin to query config

--- src/constants.rs
+++ src/constants.rs
@@ -2,4 +2,3 @@ pub const BLOCK_SIZE: usize = 1;
 pub const CONFIG_KEY: &[u8] = b"config";
 pub const MOCK_AMOUNT: u128 = 1_000_000_000_000;
 pub const MOCK_BUTT_ADDRESS: &str = "mock-butt-address";
-pub const VIEWING_KEY: &str = "DoTheRightThing.";
